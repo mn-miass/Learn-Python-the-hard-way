4e07c6e39bbb7bf750a7a24b2930b25aba057edc
M'Hamed N'Miass <mn-miass@c2r4p10.1337.ma>
2026-03-03 17:26:23 +0000

    done

---
+++ exercise 1/python.py
@@ -0,0 +1,10 @@
+import builtins
+print ("Hello World!")
+print ("Hello Again")
+print ("I like typing this.", flush = True)
+print ("This is fun.")
+print ("Yay! Printing.")
+print ("I'd much rather you 'not'.")
+print ("I \"said\" do not touch this")
+
+print (dir(builtins))
---
+++ exercise 2/python.py
@@ -0,0 +1,4 @@
+#testing comments using octothorpe this line will be ignored 
+print ("I could have code like this.")
+#skiped
+print  ("This will run.")
