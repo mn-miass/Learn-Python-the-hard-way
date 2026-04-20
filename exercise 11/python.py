0b43bf644bcdd1c562d2415782070070798cce18
M'Hamed N'Miass <mn-miass@c2r7p9.1337.ma>
2026-04-20 20:20:00 +0100

    done

---
+++ exercise 11/python.py
@@ -0,0 +1,7 @@
+print("How old are you?")
+age = input()
+print("How tall are you?")
+height = input()
+print("How much you weight?")
+weight = input()
+print(f"So you're {age} old, {height} tall and {weight} heavy")
